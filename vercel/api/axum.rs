f8686723e3e9f848da3e5c6c4f45a424e0cfbbc7
nibazshab <44338441+nibazshab@users.noreply.github.com>
2026-02-22 00:52:55 +0800

    get 部分大致结束

--- vercel/api/axum.rs
+++ vercel/api/axum.rs
@@ -1,13 +1,17 @@
 use askama::Template;
 use axum::extract::Path;
 use axum::http::Uri;
-use axum::response::{Html, IntoResponse, Response};
+use axum::response::{Html, IntoResponse, Redirect, Response};
 use axum::{
     Router,
     routing::{get, post},
 };
+use axum_extra::TypedHeader;
+use axum_extra::headers::{self, CacheControl};
 use hyper::body::Bytes;
 use hyper::{StatusCode, header};
+use rand::distr::Alphanumeric;
+use rand::{Rng, rng};
 use rust_embed::RustEmbed;
 use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
@@ -27,22 +31,40 @@ struct Note {
     content: String,
 }
 
-async fn home() -> impl IntoResponse {
+async fn root() -> Redirect {
+    Redirect::temporary(&rand_string(4))
+}
+
+async fn home(
+    Path(id): Path<String>,
+    TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
+) -> impl IntoResponse {
     let note = Note {
-        id: "1".to_string(),
+        id: id,
         content: "This is a note.".to_string(),
     };
 
-    let html = note.render().unwrap();
-    Html(html)
+    const CLI: [&str; 2] = ["curl", "wget"];
+    let is_cli = CLI.iter().any(|agent| user_agent.as_str().contains(agent));
+
+    if is_cli {
+        (
+            [(header::CONTENT_TYPE, "text/plain; charset=utf-8")],
+            note.content,
+        )
+            .into_response()
+    } else {
+        let html = note.render().unwrap();
+        Html(html).into_response()
+    }
 }
 
-async fn assets(Path(id): Path<String>) -> impl IntoResponse {
-    match Assets::get(&id) {
-        Some(file) => {
-            let content_type = if id.ends_with(".js") {
+async fn assets(Path(file): Path<String>) -> impl IntoResponse {
+    match Assets::get(&file) {
+        Some(obj) => {
+            let content_type = if file.ends_with(".js") {
                 "text/javascript"
-            } else if id.ends_with(".css") {
+            } else if file.ends_with(".css") {
                 "text/css"
             } else {
                 "application/octet-stream"
@@ -50,7 +72,7 @@ async fn assets(Path(id): Path<String>) -> impl IntoResponse {
 
             let cache_control = format!("public, max-age={}", 60 * 60 * 24 * 30 * 6); // 6 months
 
-            let bytes = match file.data {
+            let bytes = match obj.data {
                 Cow::Borrowed(slice) => Bytes::from_static(slice),
                 Cow::Owned(vec) => Bytes::from(vec),
             };
@@ -69,6 +91,20 @@ async fn assets(Path(id): Path<String>) -> impl IntoResponse {
     }
 }
 
+async fn favicon() -> impl IntoResponse {
+    (
+        [
+            (header::CONTENT_TYPE, "image/x-icon"),
+            (
+                header::CACHE_CONTROL,
+                (60 * 60 * 24 * 30 * 12).to_string().as_str(),
+            ),
+        ],
+        vec![],
+    )
+        .into_response()
+}
+
 async fn fallback(uri: Uri) -> impl IntoResponse {
     format!("Axum fallback for path {}", uri.path())
 }
@@ -76,8 +112,10 @@ async fn fallback(uri: Uri) -> impl IntoResponse {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let router = Router::new()
-        .route("/", get(home))
-        .route("/assets/{id}", get(assets))
+        .route("/", get(root))
+        .route("/{id}", get(home))
+        .route("/assets/{file}", get(assets))
+        .route("/favicon.ico", get(favicon))
         .fallback(fallback);
 
     let app = ServiceBuilder::new()
@@ -85,4 +123,12 @@ async fn main() -> Result<(), Error> {
         .service(router);
 
     vercel_runtime::run(app).await
+}
+
+fn rand_string(n: usize) -> String {
+    rng()
+        .sample_iter(&Alphanumeric)
+        .take(n)
+        .map(char::from)
+        .collect()
 }
