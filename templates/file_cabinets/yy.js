1b22d87191189a28b8ac101bae8b2cb66328c79f
nibazshab <44338441+nibazshab@users.noreply.github.com>
2026-02-26 17:08:04 +0800

    精简 一言.js

--- templates/file_cabinets/yy.js
+++ templates/file_cabinets/yy.js
@@ -1,8 +1,5 @@
 fetch("https://yyapi.xpdbk.com/api/ian?type=text")
-    .then(response => response.text())
+    .then(r => r.text())
     .then(data => {
-        document.getElementById("yy").textContent = data || "加载失败";
-    })
-    .catch(() => {
-        document.getElementById("yy").textContent = "一言加载失败";
+        document.getElementById("yy").textContent = data;
     });
